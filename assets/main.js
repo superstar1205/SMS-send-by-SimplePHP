d9e27066ac226e99abcacef7fda1ca9facfed861
Baymax <mulanohs@gmail.com>
2022-04-28 01:38:40 +0900

    Create main.js

---
+++ assets/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('.btncros').click(function() {
+        $('.msg').css({ display: 'none', });
+        $('.home').css({ margin: '36px 0px 0px 0px' })
+    });
+});
