2d815c544cd5992e1dde75cac22bda2b191753a3
Baymax <mulanohs@gmail.com>
2022-04-28 01:40:17 +0900

    Update home.php

--- main/home.php
+++ main/home.php
@@ -7,25 +7,28 @@ if (isset($_SESSION['id']) && isset($_SESSION['username'])) {
 <head>
     <title>HOME</title>
     <link rel="stylesheet" type="text/css" href="../assets/style.css">
+    <script src="../assets/jquery.js"></script>
+    <script src="../assets/main.js"></script>
 </head>
 <body>
-    <div class="home">
-        <div class="msg"> <?php echo $_SESSION['msg']; ?> </div>
-        <h1>Welcome, <?php echo $_SESSION['username']; ?>!</h1>
-        <p>Please enter all data and Sbmit.</p>
-        <form action="smssend.php" method="post" class="">
-            <input type="text" name="name" placeholder="Name" class="dev-input">
-            <input type="text" name="address" placeholder="Address" class="dev-input">
-            <input type="text" name="phone" placeholder="Phone number" class="dev-input">
-            <input type="text" name="status" placeholder="Status" class="dev-input">
-            <input type="text" name="jobdes" placeholder="Job description" class="dev-input">
-            <input type="date" name="jobdate" placeholder="Job date" class="dev-input">
-            <input type="text" name="ref" placeholder="referrence" class="dev-input">
-            <button class="submit">Submit</button>
-        </form>
-        <a class="submit logout" href="logout.php">Logout</a>
-    </div>
-    
+    <div class="container">
+    <h1>Welcome, <?php echo $_SESSION['username']; ?>!</h1>
+        <div class="home">
+            <?php if (isset ($_SESSION['msg'])) { ?> <div class="msg"><div class="msgbox"><?php echo $_SESSION['msg']; ?><label class="btncros">X</label></div></div> <?php } ?>
+            <h3>Send SMS & save data.</h3>
+            <form action="smssend.php" method="post" class="">
+                <input type="text" name="name" placeholder="Name" class="dev-input">
+                <input type="text" name="address" placeholder="Address" class="dev-input">
+                <input type="text" name="phone" placeholder="Phone number" class="dev-input">
+                <input type="text" name="status" placeholder="Status" class="dev-input">
+                <input type="text" name="jobdes" placeholder="Job description" class="dev-input">
+                <input type="date" name="jobdate" placeholder="Job date" class="dev-input">
+                <input type="text" name="ref" placeholder="referrence" class="dev-input">
+                <button class="submit">Submit</button>
+            </form>
+            <a class="logout" href="logout.php">Logout</a>
+        </div>
+    </div>    
 </body>
 </html>
 <?php 
